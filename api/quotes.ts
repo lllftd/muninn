0a2a131e4308975dcc70d37da67ec27fcf1384f1
lllftd <lllftd1@gmail.com>
2026-09-19 20:37:15 +0800

    fix: Vercel function import 去掉 .ts 扩展

--- api/quotes.ts
+++ api/quotes.ts
@@ -1,7 +1,5 @@
 import type { IncomingMessage, ServerResponse } from 'node:http'
-import { handleQuotes } from '../server/quote-fetcher.ts'
-
-export const config = { runtime: 'nodejs' }
+import { handleQuotes } from '../server/quote-fetcher'
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return handleQuotes(req, res)
